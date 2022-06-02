7c7be610c7553eb67860ea27ff7ad6146d1077d2
rafitajaen <rafitajaen@gmail.com>
2022-06-02 13:19:01 +0200

    (Curso JS Basico) Ejercicios Tema 10 - Modulos

---
+++ 03-JavaScript/exercises/10-modulos/index.js
@@ -0,0 +1,12 @@
+import * as controller from "./controller.js";
+import chalk from "chalk";
+
+/* Calling the functions suma and multiplica from the controller.js file. */
+const multiplication = controller.multiplica(
+  controller.suma(1, 2),
+  controller.suma(4, 5)
+);
+console.log("multiplication :>> ", multiplication);
+
+/* Using the chalk library to color the console.log output. */
+console.log(chalk.green("multiplication :>> ", multiplication));
