f6922d6cb75ed5b3a1549390aaee43550c3d3b0a
rafitajaen <rafitajaen@gmail.com>
2022-06-06 20:28:31 +0200

    (React Básico) - Task App hasta tema 6

---
+++ 04-React-Basico/task-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TaskListComponent from "./components/container/task_list";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <TaskListComponent></TaskListComponent>
+      </header>
+    </div>
+  );
+}
+
+export default App;
